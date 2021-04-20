b33f1337ba3bdf055c204093fc0b1461d5f7c418
Daniil Kiselev <kiselev.danil201313@gmail.com>
2021-04-20 21:32:32 +0300

    comments

--- combinatorics.py
+++ combinatorics.py
@@ -5,7 +5,7 @@ def combinations(n, k):
     result = factorial(n)/(factorial(k)*factorial(n-k))
     return result
 
-
+#сочетания с повторениями
 def repeat_combinations(n, k):
     result = factorial(n+k-1)/(factorial(k)*factorial(n-1))
     return result
@@ -14,7 +14,7 @@ def repeat_combinations(n, k):
 def permutations(n, k):
     result = factorial(n)/factorial(n-k)
     return result
-
+#размещения с повторениями
 def repeat_permutations(n, k):
     result = n**k
     return result
@@ -23,7 +23,7 @@ def repeat_permutations(n, k):
 def replacements(n):
     result = factorial(n)
     return result
-
+#перестановки с повторениями
 def repeat_replacements(n, k):
     tmp = factorial(n)
     for i in k:
